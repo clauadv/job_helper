66b1ae5d000b079e9767638926d8883c23290e5d
clau <clau.burrito@gmail.com>
2023-05-23 18:37:33 +0300

    fixed electrician

--- electrician/src/dllmain.cpp
+++ electrician/src/dllmain.cpp
@@ -25,25 +25,27 @@ bool main() {
 
 	spdlog::info("waiting for job interface");
 
+
 	for (;;) {
-		const auto pixel = GetPixel(device_context, 760, 393);
+		const auto pixel = GetPixel(device_context, 761, 393);
 
-		const shared::color::c_color<int> color{ (pixel >> 16) & 0xff, (pixel >> 8) & 0xff, pixel & 0xff };
-		if (color != shared::color::c_color<int>{ 140, 130, 124 })
-			continue;
+		shared::color::c_color<int> color{ (pixel >> 16) & 0xff, (pixel >> 8) & 0xff, pixel & 0xff };
+		if (color.r_between(100, 160) && color.g_between(110, 150) && color.b_between(110, 140)) {
+			static std::vector<std::pair<int, int>> positions = {
+				{ 757, 440 }, { 823, 440 }, { 889, 440 }, { 955, 440 }, { 1021, 440 }, { 1087, 440 }, { 1153, 440 },
+				{ 757, 560 }, { 823, 560 }, { 889, 560 }, { 955, 560 }, { 1021, 560 }, { 1087, 560 }, { 1153, 560 }
+			};
 
-		static std::vector<std::pair<int, int>> positions = {
-			{ 757, 440 }, { 823, 440 }, { 889, 440 }, { 955, 440 }, { 1021, 440 }, { 1087, 440 }, { 1153, 440 },
-			{ 757, 560 }, { 823, 560 }, { 889, 560 }, { 955, 560 }, { 1021, 560 }, { 1087, 560 }, { 1153, 560 }
-		};
+			for (const auto& position : positions) {
+				spdlog::info("simulating left click at ({}, {})", position.first, position.second);
+				shared::input::simulate_click(position);
 
-		for (const auto& position : positions) {
-			spdlog::info("simulating left click at ({}, {})", position.first, position.second);
-			shared::input::simulate_click(position);
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(50));
+				std::this_thread::sleep_for(std::chrono::milliseconds(50));
+			}
 		}
 	}
+
+	ReleaseDC(nullptr, device_context);
 
 	return false;
 }
